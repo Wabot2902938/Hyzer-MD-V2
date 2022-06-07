c1c29da84924f44fa982d2cadaeb0c6398d62d96
Wabot2902938 <106576480+Wabot2902938@users.noreply.github.com>
2022-06-07 23:22:15 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6281233738677', 'Hyzer', m)
+conn.sendContact(m.chat, '6281233738677', '𝙵𝚒𝚔𝚛𝚒𝚒 𝚂𝚔𝚢', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
